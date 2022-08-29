16632d25481f1b83d787d26afca6ccbcf26cdc51
asagafonov <a.s.agafonov@yandex.ru>
2022-08-29 19:49:50 +0300

    add map panTo on polyline fetch

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,19 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import {
-  MapContainer, TileLayer, Marker, Popup, Polyline,
+  MapContainer, TileLayer, Marker, Popup, Polyline, useMap,
 } from 'react-leaflet';
+import PropTypes from 'prop-types';
+
+const Controls = ({ coords }) => {
+  const map = useMap();
+  const zoom = 10;
+
+  useEffect(() => {
+    map.setView(coords, zoom);
+  }, [coords]);
+
+  return null;
+};
 
 const Map = () => {
-  const centerCoords = [55.751442, 37.615569];
+  const defaultCenterCoords = [55.751442, 37.615569];
+  const [centerCoords, setCenterCoords] = useState(defaultCenterCoords);
 
   const { from, to, polyline } = useSelector((state) => state.points.activeRouteData);
   const colorOptions = { color: '#407FC3' };
 
+  useEffect(() => {
+    if (polyline) {
+      setCenterCoords(polyline[Math.round(polyline.length / 2)]);
+    }
+  }, [polyline]);
+
   return (
     <MapContainer
       center={centerCoords}
-      zoom={9}
+      zoom={11}
       scrollWheelZoom={false}
       style={{ width: '100%', height: '580px' }}
     >
@@ -21,6 +40,7 @@ const Map = () => {
         attribution="&copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors"
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
+      <Controls coords={centerCoords} />
       {from && (
         <Marker position={[from.lat, from.lng]}>
           {from?.name && <Popup>{`Погрузка: ${from.name}`}</Popup>}
@@ -36,6 +56,10 @@ const Map = () => {
       )}
     </MapContainer>
   );
+};
+
+Controls.propTypes = {
+  coords: PropTypes.array,
 };
 
 export default Map;
